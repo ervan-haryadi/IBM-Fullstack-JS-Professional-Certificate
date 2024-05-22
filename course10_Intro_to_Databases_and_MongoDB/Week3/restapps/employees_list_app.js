78f53e4197763bee016abbf4d21b0c3b28a6bbca
Ervan Haryadi <ervan.a.haryadi@gmail.com>
2024-05-23 03:22:19 +0700

    add employee insertion and read endpoints

---
+++ course10_Intro_to_Databases_and_MongoDB/Week3/restapps/employees_list_app.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Employees = require('./employee');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+const uri = 'mongodb://localhost:27017';
+
+mongoose.connect(uri, { 'dbName': 'employeeDB' });
+
+// Middleware to parse JSON requests
+app.use('*', bodyParser.json());
+
+// GET endpoint to view all employees
+app.get('/api/employees', async(req, res) => {
+    const documents = await Employees.find();
+    res.json(documents);
+});
+
+// POST endpoint to add a new employee
+app.post('/api/add_employee', async(req, res) => {
+    // console.log(req);
+    let data = req.body;
+    const newEmployee = new Employees({
+        'emp_name': data['name'],
+        'age': data['age'],
+        'email': data['email'],
+        'location': data['location']
+    });
+
+    // Save new employee to database
+    await newEmployee.save();
+    res.json({message: "Employee added successfully"});
+})
+
+// run server on port 3000
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
