d31cad4751c87b3ecd8e1906a3e47733b77dabc2
Ervan Haryadi <ervan.a.haryadi@gmail.com>
2024-05-24 22:16:41 +0700

    add vendorPayment model & update package

---
+++ course10_Intro_to_Databases_and_MongoDB/Week3/paymenttracker/models/vendorPayment.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const VendorPaymentSchema = new Schema({
+    transaction_id: {
+        type: String,
+        required:true,
+        unique:true
+    },
+    vendor_id: {
+        type: String,
+        required:true,
+    },
+    invoice_no: {
+        type: Number,
+        required:true,
+    },
+    payment_duedate: {
+        type: Date,
+        default:new Date(),
+        required:true
+    },
+    amount: {
+        type: Number,
+        required:true,
+    },
+    balance_amount: {
+        type: Number,
+        required:true,
+    },
+    payment_status: {
+        type: String,
+        enum: ['Unpaid','Partially Paid','Fully Paid'],
+        required:true
+    },
+}, {timestamps: true,
+});
+
+module.exports = mongoose.model('VendorPayments', VendorPaymentSchema)
